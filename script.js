08e1bb29b55dad7ca9e05bc49a8b109562617fa6
Harry L-B <harryloefflerbell@gmail.com>
2023-09-18 15:45:54 -0400

    add board object that can display an array

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("--SCRIPT CONNECTED--");
+
+const board = (() =>{
+
+    const grid = [
+        ["X","O","X"],
+        ["O","X","O"],
+        ["X","O","X"]
+    ];
+
+    const display = () =>{
+        const boardGrid = document.querySelector("#board-grid");
+
+        for (let row =0; row <3; row++) {
+
+            let newRow = document.createElement("div");
+
+
+            for(let column = 0; column <3; column++){
+                let newGridSpot = document.createElement("div");
+                newGridSpot.textContent = grid[row][column];
+
+                newRow.appendChild(newGridSpot);
+            };
+
+            boardGrid.appendChild(newRow);
+        };
+    };
+
+    return {grid, display};
+})();
+
+board.display();
